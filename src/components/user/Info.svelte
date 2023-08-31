e2ce4c2d6a18206ab1615fb1697ea03e7816771f
PleBea <bini060315kim@gmail.com>
2023-08-31 16:02:28 +0900

    feat: Implement pass features

---
+++ src/components/user/Info.svelte
@@ -0,0 +1,77 @@
+<script>
+  import Background from "../../assets/images/pass_background.svg";
+  import { userInfo, isEmpty } from "../../stores/info";
+  import Loading from "../Loading.svelte";
+</script>
+
+<div class="info">
+  <img src={Background.src} alt="pass" width={200} height={242} />
+
+  {#if !$isEmpty}
+    <p>선린인터넷고등학교 모바일 학생증</p>
+    <div class="detail">
+      <h1>{$userInfo.name}&nbsp;</h1>
+      <h2>{$userInfo.division}&nbsp;</h2>
+      <p>{$userInfo.email}&nbsp;</p>
+    </div>
+
+    <p>{$userInfo.date}&nbsp;</p>
+  {:else}
+    <div class="loading">
+      <Loading />
+    </div>
+  {/if}
+</div>
+
+<style lang="scss">
+  .loading {
+    width: 100%;
+    height: 247px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .info {
+    background: #1c232e;
+    border-radius: 12px 12px 0 0;
+    padding: 20px;
+
+    display: flex;
+    flex-direction: column;
+    gap: 48px;
+    position: relative;
+
+    img {
+      position: absolute;
+      right: 0;
+    }
+
+    p {
+      font-weight: 400;
+      line-height: 150%;
+      color: #9babc2;
+      font-size: 15px;
+    }
+
+    h1 {
+      color: #fff;
+      font-size: 33px;
+      font-weight: 600;
+      line-height: 150%;
+    }
+
+    h2 {
+      color: #d7e4f5;
+      font-size: 17px;
+      font-weight: 600;
+      line-height: 150%;
+    }
+
+    .detail {
+      display: flex;
+      gap: 4px;
+      flex-direction: column;
+    }
+  }
+</style>
