9c09e2ad0f041d24e05b14613f3fa2743052f101
PleBea <bini060315kim@gmail.com>
2023-08-31 10:37:25 +0900

    feat: Setup pwa

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,41 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import svelte from "@astrojs/svelte";
+import AstroPWA from "@vite-pwa/astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), svelte()]
+  integrations: [
+    react(),
+    svelte(),
+    AstroPWA({
+      devOptions: {
+        enabled: true,
+        /* other options */
+      },
+      includeAssets: ["favicon.svg", "logo.svg"],
+      manifest: {
+        name: "SunrinInt = 모바일 학생증 서비스",
+        short_name: "IntPass",
+        description: "My Awesome App description",
+        icons: [
+          {
+            src: "pwa.svg",
+            sizes: "any",
+            type: "image/svg+xml",
+          },
+          // {
+          //   src: "pwa-192x192.png",
+          //   sizes: "192x192",
+          //   type: "image/png",
+          // },
+          // {
+          //   src: "pwa-512x512.png",
+          //   sizes: "512x512",
+          //   type: "image/png",
+          // },
+        ],
+      },
+    }),
+  ],
 });
