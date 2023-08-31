62f2008e7234dbf61f085f0078afee05a530b8f6
rmagur1203 <rmagur1203@naver.com>
2023-08-31 13:04:54 +0900

    disable pwa & image fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
     svelte(),
     AstroPWA({
       devOptions: {
-        enabled: true,
+        // enabled: true,
         /* other options */
       },
       includeAssets: ["favicon.svg", "logo.svg"],
